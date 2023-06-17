a7164fc2cffc036d0f8f0d176da5be1703eb2100
harrig12 <cait.harrigan@mail.utoronto.ca>
2023-06-17 19:00:57 -0400

    single-page form + cv formatting updates

--- format_bib.R
+++ format_bib.R
@@ -1,3 +1,4 @@
+library(dplyr)
 library(bibtex)
 library(htmltools)
 library(RefManageR)
--- format_entries.R
+++ format_entries.R
@@ -30,50 +30,48 @@ cv_entries <- read_sheet("1JKlSIuDrfC1wa4V1zf4Si_Tu34upiBEn1DlvFfDeFh0",
 make_buttons <- function(entry){
   buttons <- c()
   if (!is.na(entry['view'])) {
-    buttons <- c(buttons, paste0('<a href="', entry['view'],'" class="btn btn-outline-secondary icon-link"><i class="fa-solid fa-arrow-up-right-from-square"></i> view</a>'))
+    buttons <- c(buttons, paste0('<a href="', entry['view'],'" class="btn btn-outline-secondary cv-btn"><i class="fa-solid fa-arrow-up-right-from-square"></i> view</a>'))
   }
   if (!is.na(entry['pdf'])) {
-    buttons <-  c(buttons, paste0('<a href="', entry['pdf'],'" class="btn btn-outline-secondary icon-link"><i class="fa-solid fa-file-pdf"></i> pdf</a>'))
+    buttons <-  c(buttons, paste0('<a href="', entry['pdf'],'" class="btn btn-outline-secondary cv-btn"><i class="fa-solid fa-file-pdf"></i> pdf</a>'))
   }
   if (!is.na(entry['code'])) {
-    buttons <- c(buttons, paste0('<a href="', entry['code'],'" class="btn btn-outline-secondary icon-link"><i class="fa-solid fa-code"></i> code</a>'))
+    buttons <- c(buttons, paste0('<a href="', entry['code'],'" class="btn btn-outline-secondary cv-btn"><i class="fa-solid fa-code"></i> code</a>'))
   }
   return(buttons)
 }
 
 print_entry <- function(entry){
   lines <-  c('<div class="grid cv-entry">')
-  if (!is.na(entry['when'])){
-    lines <- c(lines, '<div class="g-col-1"><span class="entry-when">', entry['when'], '</span></div>')
-  }
+  lines <- c(lines, '<div class="g-col-1">')
+  # entry title
   if (!is.na(entry['line1'])){
-    lines <- c(lines, '<div class="g-col-1 g-start-2"><span class="entry-bold">', entry['line1'], '</span></div>')
+    lines <- c(lines, '<p class="entry-bold">', entry['line1'], '</p>')
   } 
   # entry may have additional lines
   if (!is.na(entry['line2'])) {
-    lines <- c(lines, '<div class="g-col-1 g-start-2"><span class="entry-plain">', entry['line2'], '</span></div>')
+    lines <- c(lines, '<p class="entry-plain">', entry['line2'], '</p>')
   }
   if (!is.na(entry['line3'])) {
-    lines <- c(lines, '<div class="g-col-1 g-start-2"><span class="entry-plain">', entry['line3'], '</span></div>')
+    lines <- c(lines, '<p class="entry-plain">', entry['line3'], '</p>')
   }
   if (!is.na(entry['line4'])) {
-    lines <- c(lines, '<div class="g-col-1 g-start-2"><span class="entry-italic">', entry['line4'], '</span></div>')
+    lines <- c(lines, '<p class="entry-italic">', entry['line4'], '</p>')
+  }
+  lines <- c(lines, '</div>')
+  if (!is.na(entry['when'])){
+    lines <- c(lines, '<div class="g-col-1"><span class="entry-when">', entry['when'], '</span>')
   }
   # entry may have buttons 
   if( any(c(!is.na(entry['view']), !is.na(entry['pdf']), !is.na(entry['pdf']))) ){
-    lines <- c(lines, '<div class="g-col-1 g-start-2">', make_buttons(entry), '</div>')
+    lines <- c(lines, make_buttons(entry))
   }
-
-  lines <- c(lines, '</div>')
+  lines <- c(lines, '</div></div>')
   HTML(lines)
 }
 
 print_section <- function(entries){
-  # hr for section header
-  hr <- '<div class="grid cv-entry"><div class="g-col-1"><div class="cv-line"></div></div></div>'
-  # section contents
-  entries <- paste(apply(entries , 1, print_entry), collapse = "")
-  paste0(hr, entries)
+  paste(apply(entries , 1, print_entry), collapse = "")
 }
 
 
